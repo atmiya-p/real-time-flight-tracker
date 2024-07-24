c5602754470b8783552116bcbdad8c42ae94d05d
atmiya-p <atmiyamp@hotmail.com>
2024-07-24 17:06:40 -0400

    Dealing with API Key

--- flight_tracker.py
+++ flight_tracker.py
@@ -1,4 +1,11 @@
-# Using AviationStack API
+# Using a flight data API called AviationStack
 
 import requests
+import config
 
+apiKey = config.API_KEY
+params = {
+    'aviation_stack_access_key' : apiKey
+}
+# Decided to use and learn the Aviation Stack API as this API is a good choice for a project with small amount of requests
+apiURL = 'http://api.aviationstack.com/v1/flights'
