9084ff88063a21df9c2c273ba057242e5b6a193b
atmiya-p <atmiyamp@hotmail.com>
2024-07-24 17:57:18 -0400

    New updates

--- flight_tracker.py
+++ flight_tracker.py
@@ -7,13 +7,20 @@ import config
 def get_flight_info(flight_number):
     apiKey = config.API_KEY
     params = {
-        'access_key' : config.API_KEY
+        'access_key' : apiKey
     }
     # Decided to use and learn the Aviation Stack API as this API is a good choice for a project with small amount of requests
     apiURL = 'http://api.aviationstack.com/v1/flights'
 
     flights_results = requests.get(apiURL, params)
-    aviationstack_info = flights_results.json()
+
+    if flights_results.ok:
+        aviationstack_info = flights_results.json()
+    else:
+        print("Unfortunately there was an error: ", flights_results.status_code)
+
+if __name__ == '__main__':
+
 
 
 
