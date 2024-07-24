79e0b14921e4822bc1a99e6da4e1e22dc2589072
atmiya-p <atmiyamp@hotmail.com>
2024-07-24 17:36:42 -0400

    Figuring out API

--- flight_tracker.py
+++ flight_tracker.py
@@ -3,9 +3,19 @@
 import requests
 import config
 
-apiKey = config.API_KEY
-params = {
-    'aviation_stack_access_key' : apiKey
-}
-# Decided to use and learn the Aviation Stack API as this API is a good choice for a project with small amount of requests
-apiURL = 'http://api.aviationstack.com/v1/flights'
+# Function to get the flight information, with the parameter being the flight number
+def get_flight_info(flight_number):
+    apiKey = config.API_KEY
+    params = {
+        'access_key' : config.API_KEY
+    }
+    # Decided to use and learn the Aviation Stack API as this API is a good choice for a project with small amount of requests
+    apiURL = 'http://api.aviationstack.com/v1/flights'
+
+    flights_results = requests.get(apiURL, params)
+    aviationstack_info = flights_results.json()
+
+
+
+
+
