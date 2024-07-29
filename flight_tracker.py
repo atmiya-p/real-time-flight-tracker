c5ea3316f2353e48e406817e4340a9b3bfff95b4
atmiya-p <atmiyamp@hotmail.com>
2024-07-28 23:25:34 -0400

    Small fixes

--- flight_tracker.py
+++ flight_tracker.py
@@ -1,10 +1,9 @@
-# Using a flight data API called AviationStack
-
 import requests
 import config
 
+
 # Function to get the flight information, with the parameter being the flight number
-def get_API_info(flightNumber):
+def get_api_info(flightNumber):
     apiKey = config.API_KEY
     params = {
         'access_key': apiKey,
@@ -21,12 +20,14 @@ def get_API_info(flightNumber):
         print("Unfortunately there was an error: ", flights_results.status_code)
         return None
 
+
 # Function to format the time that is returned by the API
 def format_time(time):
     if time:
         updatedTime = time.split("T")  # element at index 0 - time, element at index 1 - date
         return f"{updatedTime[1]}, {updatedTime[0]}"
     return "N/A"
+
 
 # Function used to display the flight information, param is the entire flight Information list that the API gives with the matched flight number
 # https://aviationstack.com/documentation
@@ -44,7 +45,7 @@ def display_flight_info(flightInformation, flightInput):
             print(f"Actual Departure Time: {format_time(flight['departure']['actual'])}, Time zone: {flight['departure']['timezone']}")
             print(f"Estimated Arrival Time: {format_time(flight['arrival']['estimated'])}, Time zone: {flight['arrival']['timezone']}")
             print(f"Actual Arrival Time: {format_time(flight['arrival']['actual'])}, Time zone: {flight['arrival']['timezone']}")
-            
+
         else:
             print(f"Flight {flightInput} could not be found")
     else:
@@ -54,7 +55,7 @@ def display_flight_info(flightInformation, flightInput):
 if __name__ == '__main__':
     flightInput = input("Enter the flight you'd like to track: ")
 
-    flightInformation = get_API_info(flightInput)
+    flightInformation = get_api_info(flightInput)
 
     if flightInformation:
         display_flight_info(flightInformation, flightInput)
